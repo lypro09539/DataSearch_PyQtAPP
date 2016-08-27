c7387b8edbecbbf53855dd2e47bc4aa20313c7e3
lypro09539 <772907490@qq.com>
2016-08-27 23:58:45 +0800

    ex48

---
+++ ex48_skeleton/ex46/game.py
@@ -0,0 +1,17 @@
+class lexicon(object):
+
+    def __init__(self, stuff):
+        pass
+        # stuff = raw_input('>')
+        #words = lex.spilt()
+
+    def scan(self):
+        
+        stuff = raw_input('>')
+        words = lex.spilt()
+        for word in words:
+            if word = 'go' or 'kill' or 'eat'
+                result + = ['verb', %s] %word
+            elif word = 'the' or 'in' or 'of'
+                result + = ['stop', %s] %word
+                
